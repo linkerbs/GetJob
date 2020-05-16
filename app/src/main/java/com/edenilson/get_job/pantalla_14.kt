0a65208c0f49481b41345b6e619841af93d1583d
Nison6 <49014335+Nison6@users.noreply.github.com>
2020-05-16 16:44:59 -0600

    Pantallas Usuario, Empresa
    Pantallas Usuario, Empresa

---
+++ app/src/main/java/com/edenilson/get_job/pantalla_14.kt
@@ -0,0 +1,22 @@
+package com.edenilson.get_job
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class pantalla_14 : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_pantalla_14, container, false)
+    }
+
+}
---
+++ app/src/main/java/com/edenilson/get_job/pantalla_20.kt
@@ -0,0 +1,12 @@
+package com.edenilson.get_job
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class pantalla_20 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pantalla_20)
+    }
+}
