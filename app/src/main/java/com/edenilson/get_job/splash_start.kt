1cc445c9c8c2398c796f58b1c6f73ac43354a940
DouglasLino <00018318@uca.edu.sv>
2020-06-17 18:31:47 -0600

    Add logo y splash

---
+++ app/src/main/java/com/edenilson/get_job/splash_start.kt
@@ -0,0 +1,12 @@
+package com.edenilson.get_job
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class splash_start : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_start)
+    }
+}
