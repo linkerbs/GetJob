5f798ef2e5e1ca081f17a09bd76cac45b6cfd432
DouglasLino <00018318@uca.edu.sv>
2020-07-10 05:08:57 -0600

    muestra perfil de empresa

--- app/src/main/java/com/edenilson/get_job/CompanyActivity.kt
+++ app/src/main/java/com/edenilson/get_job/CompanyActivity.kt
@@ -2,12 +2,24 @@ package com.edenilson.get_job
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
+private const val TAG: String = "CompanyActivity"
 class CompanyActivity : AppCompatActivity() {
+
+    val firebaseFirestore: FirebaseFirestore = FirebaseFirestore.getInstance()
+    //    Esto es para utilizarlo con el modelView con la data del usuario
+    private var modelPerfil: UserActivity.getPerfil? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,10 +32,96 @@ class CompanyActivity : AppCompatActivity() {
 
         findViewById<BottomNavigationView>(R.id.bottomNav)
             .setupWithNavController(NavController)
+
+        //        llevo la informacion al fragmento
+
+        modelPerfil = ViewModelProviders.of(this).get(UserActivity.getPerfil::class.java)
+
+        val user = FirebaseAuth.getInstance().currentUser
+        user?.let {
+            val correo = user.email
+            Log.d(TAG,"Usuario: ${user.uid}" )
+
+            //    Obtengo la informacion de la oferta laboral
+            if (correo != null) {
+
+                firebaseFirestore.collection("usuarios")
+                    .whereEqualTo("correo", correo)
+                    .get()
+                    .addOnSuccessListener { documents ->
+                        for (document in documents) {
+                            Log.d(TAG, "${document.id} => ${document.data}")
+//                            binding.tvNombreUsuario.text = document.getString("nombre")
+//                            Glide.with(this).load(document.getString("foto")).into(binding.imgBtnPerfilUsuario)
+                            document.getString("nombre")?.let { it1 -> modelPerfil?.nombre(it1) }
+                            document.getString("foto")?.let { it1 -> modelPerfil?.foto(it1) }
+//                            modelPerfil?.nombre(document.getString("nombre"))
+                        }
+                    }
+                    .addOnFailureListener { exception ->
+                        Log.w(TAG, "Error getting documents for CompanyActivity: ", exception)
+                    }
+            }
+
+
+        }
+
+
     }
 
     override fun onSupportNavigateUp(): Boolean {
         val NavController = this.findNavController(R.id.MyNavHostFragment2)
         return NavController.navigateUp()
     }
+
+    //    cuando le atras se salga de la app
+    private val INTERVALO = 2000 //2 segundos para salir
+    private var tiempoPrimerClick: Long = 0
+    override fun onBackPressed() {
+        if (tiempoPrimerClick + INTERVALO > System.currentTimeMillis()) {
+//            super.onBackPressed()
+            finishAffinity()
+
+            return
+        } else {
+            Toast.makeText(this, "Vuelve a presionar para salir", Toast.LENGTH_SHORT).show()
+        }
+        tiempoPrimerClick = System.currentTimeMillis()
+    }
+
+
+
+
+
+
+
+
+    //    viewmodel para pasar los datos a la pantalla_16
+        class getPerfil : ViewModel() {
+
+
+            val _nombre = MutableLiveData<Any>()
+            val _correo = MutableLiveData<Any>()
+            val _foto = MutableLiveData<Any>()
+
+
+            fun nombre(msg: kotlin.String) {
+                _nombre.setValue(msg)
+
+            }
+
+            fun correo(msg: kotlin.String) {
+                _correo.setValue(msg)
+
+            }
+
+            fun foto(msg: kotlin.String) {
+                _foto.setValue(msg)
+
+            }
+        }
+
+
+
+
 }
--- app/src/main/java/com/edenilson/get_job/pantalla_16.kt
+++ app/src/main/java/com/edenilson/get_job/pantalla_16.kt
@@ -7,7 +7,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.findNavController
+import com.bumptech.glide.Glide
 import com.edenilson.get_job.databinding.FragmentPantalla16Binding
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -16,6 +18,8 @@ import com.google.firebase.ktx.Firebase
  * A simple [Fragment] subclass.
  */
 class pantalla_16 : Fragment() {
+    //    Esto es para utilizarlo con el modelView con la data del usuario
+    private var modelPerfil: UserActivity.getPerfil? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,7 +29,7 @@ class pantalla_16 : Fragment() {
         val binding = DataBindingUtil.inflate<FragmentPantalla16Binding>(
             inflater, R.layout.fragment_pantalla_16
             , container, false)
-        binding.editText6.setOnClickListener { view: View ->
+        binding.btnModificarPerfilUsuario.setOnClickListener { view: View ->
             view.findNavController().navigate(R.id.action_pantalla_17_to_pantalla_172)
 
         }
@@ -35,7 +39,20 @@ class pantalla_16 : Fragment() {
             val intent = Intent(activity, MainActivity::class.java)
             activity!!.startActivity(intent)
         }
+
         (activity as CompanyActivity).supportActionBar?.title = ("Perfil")
+
+//        Cargamos los datos del perfil desde el viewmodel
+        modelPerfil = ViewModelProviders.of(activity!!).get(UserActivity.getPerfil::class.java)
+
+        //        Esto es para utilizarlo con modelView
+//        los sampo a los Input
+        binding.tvNombreEmpresa?.text = modelPerfil!!._nombre.value.toString()
+//        Aqui iria la descripcion completa del usuario, hay que agregar al viewm model el campo
+//        binding.tvDescripcionCompletaUsuario.text = modelPerfil!!._nombre.value.toString()
+        val imagen = modelPerfil!!._foto.value.toString()
+        binding.imgFotoEmpresa?.let { Glide.with(this).load(imagen).into(it) }
+
         return binding.root
     }
 
