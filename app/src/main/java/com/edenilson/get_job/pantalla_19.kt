aaaa0059ac4de156981c5f019aeea504af242019
linkerbs <00002918@uca.edu.sv>
2020-07-13 18:20:02 -0600

    Se guarda el cv en la coleccion

--- app/src/main/java/com/edenilson/get_job/pantalla_19.kt
+++ app/src/main/java/com/edenilson/get_job/pantalla_19.kt
@@ -1,6 +1,7 @@
 package com.edenilson.get_job
 
 import android.app.Activity
+import android.content.ContentValues
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
@@ -8,8 +9,11 @@ import android.provider.MediaStore
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.INVISIBLE
+import android.view.View.VISIBLE
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
@@ -44,12 +48,8 @@ class pantalla_19 : Fragment() {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == 0 && resultCode == Activity.RESULT_OK && data != null) {
             selectedPhoto = data.data
-
-            Toast.makeText(
-                activity,selectedPhoto.toString(),
-                Toast.LENGTH_SHORT
-            ).show()
-            btPdf.text = "Seleccionado"
+            btPdf.visibility = INVISIBLE
+            btPdf2.visibility = VISIBLE
         }
     }
 
@@ -74,10 +74,10 @@ class pantalla_19 : Fragment() {
 
         binding.btCerrar.setOnClickListener { view: View ->
 
-           /* Firebase.auth.signOut()
+            Firebase.auth.signOut()
             val intent = Intent(activity, MainActivity::class.java)
-            activity!!.startActivity(intent)*/
-            pdfUpload()
+            activity!!.startActivity(intent)
+
         }
 
         //        Esto es para utilizarlo con modelView
@@ -109,17 +109,20 @@ class pantalla_19 : Fragment() {
         val imagen = modelPerfil!!._foto.value.toString()
         binding.imgFotoPerfil.let { Glide.with(this).load(imagen).into(it) }
 
+
+
         binding.btPdf.setOnClickListener { view: View ->
             val intent = Intent(Intent.ACTION_GET_CONTENT)
             intent.type = "application/pdf"
             startActivityForResult(intent, 0)
-
-
-
         }
 
 
 
+
+        binding.btPdf2.setOnClickListener {
+            pdfUpload()
+        }
 
 
         return binding.root
@@ -128,32 +131,46 @@ class pantalla_19 : Fragment() {
     }
 
 
-   private fun pdfUpload() {
+    private fun pdfUpload() {
 
 
-       var devolver: String = ""
-       val db = FirebaseFirestore.getInstance()
+        var devolver: String = ""
+        val db = FirebaseFirestore.getInstance()
 
-       val user = FirebaseAuth.getInstance().currentUser
-       user?.let {
-           val correo = user.email
-           val ref = FirebaseStorage.getInstance().getReference("/cv/$correo")
+        val user = FirebaseAuth.getInstance().currentUser
+        user?.let {
+            val correo = user.email
+            val filename = UUID.randomUUID().toString()
+            val ref = FirebaseStorage.getInstance().getReference("/cv/$filename")
 
-           ref.putFile(selectedPhoto!!).addOnSuccessListener { it ->
-               Log.d("Registro", "Se subio la imagen: ${it.metadata?.path}")
-
-               ref.downloadUrl.addOnSuccessListener {
-                   val doc = db.collection("usuarios").document(correo!!)
-                   doc.update("cv", it)
-                   .addOnSuccessListener { }
-                   .addOnFailureListener {  }
+            ref.putFile(selectedPhoto!!).addOnSuccessListener { it ->
+                Log.d("Registro", "Se subio la imagen: ${it.metadata?.path}")
+                ref.downloadUrl.addOnSuccessListener {
 
 
+                     db.collection("usuarios")
+                          .whereEqualTo("correo",correo)
+                          .get()
+                          .addOnSuccessListener { documents ->
+                              for(document in documents){
+                                  Log.d("Registro", "Se subio la imagen: ${it}")
+                                  db.collection("usuarios").document(document.id)
+                                      .update(
+                                          "cv" , it.toString()
+                                      )
+                              }
+                          }.addOnFailureListener { e ->
+                              Log.w(
+                                  ContentValues.TAG,
+                                  "Exploto el pdf",
+                                  e
+                              )
+                          }
 
-               }
-           }
+                }
+            }
 
-       }
-   }
+        }
+    }
 
 }
